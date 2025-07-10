f99ea40ebcd047f2009a345c08e56f3bb9746c2d
Mrak140 <60341685+Mrak140@users.noreply.github.com>
2025-07-10 08:16:13 +0700

    добавил идентификацию цифрами для областей

--- main.go
+++ main.go
@@ -5,24 +5,39 @@ import (
 	"log"
 	"math"
 	"math/rand"
+	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
-	screenWidth  = 800
-	screenHeight = 600
-	numSites     = 50 // Количество точек (сайтов)
+	screenWidth  = 1920
+	screenHeight = 1080
+	numSites     = 100
 )
+
+// Палитра цветов для цифр 1-10
+var digitColors = []color.RGBA{
+	{255, 0, 0, 255},     // 1: Красный
+	{0, 255, 0, 255},     // 2: Зелёный
+	{0, 0, 255, 255},     // 3: Синий
+	{255, 255, 0, 255},   // 4: Жёлтый
+	{255, 0, 255, 255},   // 5: Пурпурный
+	{0, 255, 255, 255},   // 6: Бирюзовый
+	{128, 0, 0, 255},     // 7: Тёмно-красный
+	{0, 128, 0, 255},     // 8: Тёмно-зелёный
+	{0, 0, 128, 255},     // 9: Тёмно-синий
+	{128, 128, 128, 255}, // 10: Серый
+}
 
 type Game struct {
 	sites []Point
 }
 
 type Point struct {
-	x, y float64
-	col  color.RGBA
+	x, y  float64
+	digit int
 }
 
 func NewGame() *Game {
@@ -31,19 +46,13 @@ func NewGame() *Game {
 	return g
 }
 
-// Генерация случайных точек
 func (g *Game) generateRandomSites(count int) {
 	g.sites = make([]Point, count)
 	for i := 0; i < count; i++ {
 		g.sites[i] = Point{
-			x: rand.Float64() * screenWidth,
-			y: rand.Float64() * screenHeight,
-			col: color.RGBA{
-				R: uint8(rand.Intn(200) + 55),
-				G: uint8(rand.Intn(200) + 55),
-				B: uint8(rand.Intn(200) + 55),
-				A: 255,
-			},
+			x:     rand.Float64() * screenWidth,
+			y:     rand.Float64() * screenHeight,
+			digit: rand.Intn(10) + 1,
 		}
 	}
 }
@@ -56,9 +65,35 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	// Отрисовка диаграммы Вороного
 	g.drawVoronoi(screen)
 
-	// Отрисовка точек (сайтов)
+	// Отрисовка цифр
 	for _, site := range g.sites {
-		ebitenutil.DrawCircle(screen, site.x, site.y, 4, color.Black)
+		text := strconv.Itoa(site.digit)
+		textX := int(site.x) - 5*len(text)/2
+		textY := int(site.y) + 5
+		ebitenutil.DebugPrintAt(screen, text, textX, textY)
+	}
+}
+
+func (g *Game) drawVoronoi(screen *ebiten.Image) {
+	for y := 0; y < screenHeight; y++ {
+		for x := 0; x < screenWidth; x++ {
+			minDist := math.MaxFloat64
+			closestDigit := 1
+
+			for _, site := range g.sites {
+				dx := float64(x) - site.x
+				dy := float64(y) - site.y
+				dist := dx*dx + dy*dy
+
+				if dist < minDist {
+					minDist = dist
+					closestDigit = site.digit
+				}
+			}
+
+			// Берём цвет из палитры по цифре (closestDigit - 1, т.к. индексы с 0)
+			screen.Set(x, y, digitColors[closestDigit-1])
+		}
 	}
 }
 
@@ -68,33 +103,8 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Voronoi Diagram in Ebiten")
+	ebiten.SetWindowTitle("Voronoi Diagram: One Color per Digit")
 	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
-	}
-}
-
-// Наивный алгоритм Вороного (перебор всех пикселей)
-func (g *Game) drawVoronoi(screen *ebiten.Image) {
-	for y := 0; y < screenHeight; y++ {
-		for x := 0; x < screenWidth; x++ {
-			// Находим ближайшую точку
-			minDist := math.MaxFloat64
-			var closestSite Point
-
-			for _, site := range g.sites {
-				dx := float64(x) - site.x
-				dy := float64(y) - site.y
-				dist := dx*dx + dy*dy // Квадрат расстояния (оптимизация)
-
-				if dist < minDist {
-					minDist = dist
-					closestSite = site
-				}
-			}
-
-			// Закрашиваем пиксель цветом ближайшей точки
-			screen.Set(x, y, closestSite.col)
-		}
 	}
 }
